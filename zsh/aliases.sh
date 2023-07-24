3483cfb810fbe01b9c7907e4078aa5346dc150c2
rnoba <rnoba.iwb@gmail.com>
2023-07-24 16:38:55 -0300

    UPPPP

---
+++ zsh/aliases.sh
@@ -0,0 +1,7 @@
+#ALIASES
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --tree --icons --git-ignore --git --list-dirs"
+alias v="nvim"
+alias cat="bat --style=plain --theme='gruvbox-dark'"
+alias daily="v $HOME/Iwb/daily.md"
+# END
---
+++ zsh/zsh-defaults.sh
@@ -0,0 +1,7 @@
+# Lines configured by zsh-newuser-install
+HISTFILE="$HOME/.histfile"
+HISTSIZE=1000
+SAVEHIST=1000
+setopt extendedglob nomatch notify
+bindkey -v
+# End of lines configured by zsh-newuser-install
