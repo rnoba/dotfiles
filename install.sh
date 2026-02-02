0cc8e99310d9f4258769b572d17704576a39c1fb
Rafael Barros <130116482+rnoba@users.noreply.github.com>
2026-02-02 18:14:07 -0300

    Fix symlink for nix-daemon in setup_nix function

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ safe_link() {
 setup_nix() {
 	mkdir -p "$HOME/.local/share/nix"
 	sudo xbps-install -Sy nix
-	sudo ln -s /etc/sv /var/service
+	sudo ln -s /etc/sv/nix-daemon /var/service
 	sudo cp ./nix.sh /etc/profile.d/nix.sh
 
 	nix-channel --add http://nixos.org/channels/nixpkgs-unstable
