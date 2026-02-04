9f77e1c7e9b16f9b0fcf1b2e379b2adf199f6ac6
rnoba (Rafael Barros) <rnoba.iwb@gmail.com>
2026-02-04 17:07:13 -0300

    up

---
+++ config/mozilla/firefox/profiles/rnoba/user.js
@@ -0,0 +1,42 @@
+user_pref("sidebar.revamp", true);
+user_pref("sidebar.verticalTabs", true);
+
+user_pref("gfx.webrender.all", true);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.hardware-video-decoding.enabled", true);
+user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("dom.webgpu.enabled", true);
+
+user_pref("ui.prefersReducedMotion", 1);
+
+user_pref("dom.security.https_only_mode", true);
+user_pref("dom.security.https_only_mode_ever_enabled", true);
+
+user_pref("browser.compactmode.show", true);
+user_pref("browser.uidensity", 1);
+
+user_pref("browser.startup.page", 3);
+user_pref("browser.sessionstore.resume_from_crash", true);
+
+user_pref("nglayout.initialpaint.delay", 0);
+user_pref("nglayout.initialpaint.delay_in_oopif", 0);
+
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.startup.homepage", "about:blank");
+
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+
+user_pref("browser.download.useDownloadDir", false);
+
+user_pref("browser.tabs.warnOnClose", true);
+
+user_pref("pdfjs.disabled", false);
+user_pref("pdfjs.enableScripting", false);
+
+user_pref("media.eme.enabled", true);
+
+user_pref("media.autoplay.default", 5);
+
+user_pref("full-screen-api.warning.timeout", 0);
