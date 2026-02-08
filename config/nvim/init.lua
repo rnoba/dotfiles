8cb3de3d2f65b43d29df1d82ef72de072f5808d0
rnoba (Rafael Barros) <rnoba.iwb@gmail.com>
2026-02-08 15:51:31 -0300

    .

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.g.have_nerd_font = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "100"
